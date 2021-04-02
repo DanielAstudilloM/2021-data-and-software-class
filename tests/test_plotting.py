3b165dc14195c8c52f8015a052fea6036eaf629b
Daniel Astudillo <ret640@gmail.com>
2021-04-02 19:09:32 -0400

    tests and assertions

---
+++ tests/test_plotting.py
@@ -0,0 +1,9 @@
+import sys,os
+import numpy as np
+
+sys.path.append(os.path.join(os.path.dirname(__file__),"../"))
+
+import src.plotting as plotting
+
+def test_plot():
+    assert(plotting.plot() == None)
